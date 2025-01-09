cbe08784a28c7310e02c526c1d28e5b178dce355
Peter Gibb <pgibb96@gmail.com>
2025-01-09 17:12:46 -0500

    Fixing initialization

--- server.go
+++ server.go
@@ -6,36 +6,32 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/pgibb96/game_stats/proto"
+	"github.com/pgibb96/game-stats/proto" // Import the generated proto package
 	"google.golang.org/grpc"
 )
 
-// Define the server that implements the gRPC service
 type server struct {
-	pb.UnimplementedMyServiceServer
+	proto.UnimplementedMyServiceServer // Embed the generated server interface
 }
 
-// Implement the SayHello RPC method
-func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
-	return &pb.HelloResponse{Message: fmt.Sprintf("Hello, %s!", req.Name)}, nil
+func (s *server) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
+	// Implement the SayHello method from the service
+	return &proto.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
 }
 
 func main() {
-	// Create a listener on port 50051
+	// Start the gRPC server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	// Create a new gRPC server
-	grpcServer := grpc.NewServer()
+	s := grpc.NewServer()
+	proto.RegisterMyServiceServer(s, &server{}) // Register the service with the gRPC server
 
-	// Register the service with the server
-	pb.RegisterMyServiceServer(grpcServer, &server{})
-
-	// Start the server
-	log.Println("gRPC server listening on port 50051...")
-	if err := grpcServer.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
